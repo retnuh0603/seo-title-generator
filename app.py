e62b741c8ce09660f769d99a03f18ff000bab769
Hunter Van Ry <hunter.vanry@gmail.com>
2026-06-11 18:15:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ st.set_page_config(page_title="SEO Title Tag Generator")
 
 st.title("Bulk SEO Title Tag Generator")
 
+st.header("By SEOAirman")
+
 st.write(
     "Paste your URLs below. Each page will be crawled and a new, "
     "perfectly optimized title tag will be written. You will have the "
