b83f3a27c75420c9d39860f8747683cc0895d5a5
Hunter Van Ry <hunter.vanry@gmail.com>
2026-06-11 22:06:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="SEO Title Tag Generator")
 
 st.title("Bulk SEO Title Tag Generator", text_alignment="center") 
 with st.container(horizontal_alignment="center"):
-    st.image("hunterlogoblack.svg", width="content")
+    st.image("hunterlogoblack.svg", width="100")
 
 
 st.write(
