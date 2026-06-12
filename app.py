74f4e94efc39cfa1d6e727143cb5d4fb2079ed6e
Hunter Van Ry <hunter.vanry@gmail.com>
2026-06-11 22:03:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from openai import OpenAI
 st.set_page_config(page_title="SEO Title Tag Generator")
 
 st.title("Bulk SEO Title Tag Generator", text_alignment="center") 
-st.subheader("By SEOAirman", text_alignment="center")
 with st.container(horizontal_alignment="center"):
     st.image("hunterlogoblack.svg")
 
