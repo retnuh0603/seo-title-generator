a640c1391acb4199cbf72a53fa36acd3ff926c99
Hunter Van Ry <hunter.vanry@gmail.com>
2026-06-12 09:31:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ st.set_page_config(page_title="SEO Title Tag Generator")
 
 hide_st_style = """
             <style>
-            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
